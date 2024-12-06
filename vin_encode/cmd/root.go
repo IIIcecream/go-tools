552f840232366e611371211b8beec03b125ca965
tangwei20 <tangwei20@baidu.com>
2024-12-06 16:58:58 +0800

    添加version信息

--- vin_encode/cmd/root.go
+++ vin_encode/cmd/root.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/IIIcecream/version"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:  "vin_cli",
-	Long: `vin号加密解密工具`,
+	Use:     "vin_cli",
+	Long:    `vin号加密解密工具`,
+	Version: version.FullVersion(),
 }
 
 func Execute() {
