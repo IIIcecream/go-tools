2c60499ea97f5ab2469737d76385c695dfe9e41c
tangwei20 <tangwei20@baidu.com>
2024-12-06 11:25:52 +0800

    修改import路径

--- vin_encode/cmd/root.go
+++ vin_encode/cmd/root.go
@@ -7,19 +7,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	encodeString string
-	decodeString string
-)
-
 var rootCmd = &cobra.Command{
 	Use:  "vin_cli",
 	Long: `vin号加密解密工具`,
 }
 
 func Execute() {
-	rootCmd.Flags().StringVarP(&encodeString, "string", "e", "L6T79P4N7RD091732", "vin")
-	rootCmd.Flags().StringVarP(&decodeString, "string", "d", "dB4dgIsvjWgbZLw", "encoded vin")
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- vin_encode/main.go
+++ vin_encode/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "go-tools/vin_encode/cmd"
+import "github.com/IIIcecream/go-tools/vin_encode/cmd"
 
 func main() {
 	cmd.Execute()
